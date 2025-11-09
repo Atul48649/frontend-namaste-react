a74936bdc695079fbac088fdd0f609d17f2aa4a9
Atul Kumar <atul48649@gmail.com>
2025-11-09 18:34:13 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-const heading = React.createElement('h1', {
-    id: 'heading'
-}, 'Hello World from React!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = (React.createElement('h1', { id: 'heading' }, "Namaste React!"));
+
+// const heading = (<h1 className="heading">Namaste React from JSX!</h1>);
+
+const Title = ()=>(
+    <h1 className="head">Namaste React </h1>
+)
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+)
+
+const rooot = ReactDOM.createRoot(document.getElementById('root'));
+
+rooot.render(<HeadingComponent/>);
